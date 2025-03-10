48074a02860fb29eed191d76412d022816cc84e0
ounjuu <koj1555@naver.com>
2025-03-10 15:14:00 +0900

    Feat: naver kakao login

--- public/js/login.js
+++ public/js/login.js
@@ -78,44 +78,100 @@ const goToFindPasswordPage = () => {
 };
 
 // 네이버 로그인 axios
-// document
-//   .getElementById("naver-login-btn")
-//   .addEventListener("click", function () {
-//     window.location.href = "http://localhost:3000/user/login/naver";
-//   });
+document
+  .getElementById("naver_id_login")
+  .addEventListener("click", function () {
+    window.location.href = "http://localhost:3000/user/login/naver";
+  });
 
-// const params = new URLSearchParams(window.location.search);
-// const code = params.get("code");
-// const state = params.get("state");
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+const state = params.get("state");
 
-// axios
-//   .post("http://localhost:3000/user/login/naver/callback", { code, state })
-//   .then((response) => {
-//     console.log("네이버 로그인 성공:", response.data);
-//   })
-//   .catch((error) => {
-//     console.error("네이버 로그인 실패:", error);
-//   });
+axios
+  .post("http://localhost:3000/user/login/naver/callback", { code, state })
+  .then((response) => {
+    console.log("네이버 로그인 성공:", response.data);
+  })
+  .catch((error) => {
+    console.error("네이버 로그인 실패:", error);
+  });
 
 // 카카오 로그인
-// Kakao.init(process.env.KAKAO_JS_KEY);
-// console.log(Kakao.isInitialized());
+// main.js
 
-function kakaoLogin() {
+// 카카오 초기화 (복사한 JavaScript 키 넣기)
+Kakao.init("KAKAO_JS_KEY");
+console.log(Kakao.isInitialized());
+
+fetch("/get-key")
+  .then((response) => response.json())
+  .then((data) => {
+    Kakao.init(data.kakaoKey);
+    console.log(Kakao.isInitialized()); // true면 초기화 성공
+  })
+  .catch((error) => console.error("Error:", error));
+
+// main.js
+
+fetch("/get-key")
+  .then((response) => response.json())
+  .then((data) => {
+    Kakao.init(data.kakaoKey); // 서버로부터 받은 키로 초기화
+    console.log(Kakao.isInitialized()); // true면 초기화 성공
+  })
+  .catch((error) => console.error("Error:", error));
+
+// 로그인 버튼 클릭 이벤트
+document.getElementById("kakao-login-btn").addEventListener("click", () => {
   Kakao.Auth.login({
-    success: function (response) {
+    success: function (authObj) {
+      console.log(authObj); // 로그인 성공 정보 출력
+
+      // 사용자 정보 요청
       Kakao.API.request({
         url: "/v2/user/me",
-        success: function (response) {
-          console.log(response);
+        success: function (res) {
+          console.log(res);
+
+          const nickname = res.properties.nickname; // 닉네임 정보
+          const email = res.kakao_account.email; // 이메일 정보
+
+          const userInfo = `
+                        <p>닉네임: ${nickname}</p>
+                        <p>이메일: ${email}</p>
+                    `;
+          document.getElementById("user-info").innerHTML = userInfo;
+
+          // 서버로 로그인 정보 보내기
+          fetch("/user/login/kakao", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              accessToken: authObj.access_token,
+              email: email,
+              nickname: nickname,
+            }),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+
+              // 로그인 성공 메시지 표시
+              alert(`${nickname}님 환영합니다!`);
+
+              // 메인 페이지로 이동 (예: /main 으로 이동)
+              window.location.href = "/main";
+            })
+            .catch((error) => console.error("Error:", error));
         },
         fail: function (error) {
-          console.log(error);
+          console.error("사용자 정보 요청 실패:", error);
         },
       });
     },
-    fail: function (error) {
-      console.log(error);
+    fail: function (err) {
+      console.error("로그인 실패:", err);
     },
   });
-}
+});
